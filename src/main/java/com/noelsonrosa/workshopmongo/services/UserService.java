d5439ad8c78daf694a90d831ec27192ee73c3baa
Noelson-Rosa <noelson.nr@gmail.com>
2022-01-14 18:40:29 -0300

    obtendo um usuario por id

--- src/main/java/com/noelsonrosa/workshopmongo/services/UserService.java
+++ src/main/java/com/noelsonrosa/workshopmongo/services/UserService.java
@@ -7,15 +7,28 @@ import org.springframework.stereotype.Service;
 
 import com.noelsonrosa.workshopmongo.domain.User;
 import com.noelsonrosa.workshopmongo.repository.UserRepository;
+import com.noelsonrosa.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
-		return repo.findAll(); 
-		
-	}	
+
+	public List<User> findAll() {
+		return repo.findAll();
+
+	}
+	public User findById(String id) {
+		java.util.Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	/*versão antiga não rodou ----- public User findById(String id) {
+		User user = repo.findOne(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}*/
+
 }
